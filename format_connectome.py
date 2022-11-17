ba74399b5bbcf8b6faf2be3283252dfadd2805cb
Christopher Lin <christopher.lin045@gmail.com>
2022-11-17 13:53:42 -0500

    fix filepaths

--- format_connectome.py
+++ format_connectome.py
@@ -2,7 +2,7 @@ import glob
 import multiprocessing
 import numpy as np
 from nilearn import image, maskers
-from tqdm import tqdm 
+from tqdm import tqdm
 from natsort import natsorted
 import argparse
 import os
@@ -12,7 +12,7 @@ import os
 # Function that transforms a set of nifti files to an npy file and a norms file for use with connectome_quick
 # args is a tuple in the form (files[], subject_name, mask_file, output_dir)
 def transform(args):
-    files = args[0] 
+    files = args[0]
     subject_name = args[1]
     mask_file = args[2]
     output_dir = args[3]
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # All NIMLAB analyses have used the FSL 2mm_brain_mask_dil file since shifting to python code (older analyses used the 222.nii.gz mask as in Lead-DBS)
     mask_img = "./MNI152_T1_2mm_brain_mask_dil.nii.gz"
 
-    files = natsorted(glob.glob(os.path.join(input_dir,"sub*/vol/sub*.nii.gz")))
+    files = natsorted(glob.glob(os.path.join(input_dir,"sub*/func/sub*.nii.gz")))
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -51,12 +51,12 @@ if __name__ == "__main__":
 
     subject_args = []
     for s in unique_subjects:
-        runs = natsorted(glob.glob(os.path.join(input_dir,s+'*','vol',s+'*')))
+        runs = natsorted(glob.glob(os.path.join(input_dir,s+'*','func',s+'*')))
         subject_args.append((runs, s, mask_img, output_dir))
 
     # show the results for the first five subjects
     print(subject_args[:5])
-                        
+
     # make sure we got everyone
     number_of_subjects = len(subject_args)
     print(number_of_subjects)
